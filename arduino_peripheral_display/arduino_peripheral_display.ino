9cd5f76a6ce1340488f459dff02abe10da491d0d
SamJackson24816 <samj24816@gmail.com>
2024-10-09 21:57:16 -0400

    Cleaned up arduino code

--- arduino_peripheral_display/arduino_peripheral_display.ino
+++ arduino_peripheral_display/arduino_peripheral_display.ino
@@ -1,25 +1,4 @@
-// Define the buzzer pin
-const int buzzerPin = 3;
 
-// Define the notes and their frequencies
-#define NOTE_C4 262
-#define NOTE_D4 294
-#define NOTE_E4 330
-#define NOTE_F4 349
-#define NOTE_G4 392
-#define NOTE_A4 440
-#define NOTE_B4 494
-#define NOTE_C5 523
-
-// Define the melody
-int melody[] = {
-  NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_C5
-};
-
-// Define the duration of each note (in milliseconds)
-int noteDuration = 500;
-
-// Define the Command enum first so it's recognized everywhere
 enum Command {
   SIGNED_IN,
   SIGNED_OUT,
@@ -27,12 +6,6 @@ enum Command {
   NONE
 };
 
-
-void setup() {
-  // Set the buzzer pin as an output
-  pinMode(buzzerPin, OUTPUT);
-  Serial.begin(9600);
-}
 
 class OutputIO {
   public:
@@ -160,17 +133,22 @@ class OutputIOLED : public OutputIO {
     }
 };
 
-Command command = Command::NONE;
-OutputIO* output = new OutputIOLED();  // Instantiate a derived class object
 
-
-
-// Declare function prototypes so they can be called in loop() before being defined
 Command update(OutputIO& output, Command command);
 Command parseInput(String input);
 
+
+
+Command command = Command::NONE;
+OutputIO* output = new OutputIOLED();
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
 void loop() {
-  command = update(*output, command);  // Pass output by reference
+  command = update(*output, command);
 }
 
 // Define the update function
