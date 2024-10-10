2e221465795803d8cf1985dcce7411df1b45ce97
SamJackson24816 <samj24816@gmail.com>
2024-10-09 21:55:06 -0400

    Added arduino LED pattern configuration

--- arduino_peripheral_display/arduino_peripheral_display.ino
+++ arduino_peripheral_display/arduino_peripheral_display.ino
@@ -36,6 +36,7 @@ void setup() {
 
 class OutputIO {
   public:
+    virtual void createResources() = 0;
     virtual void startCommand(Command command) = 0;
     virtual void runCommand() = 0;
     virtual bool isCommandFinished() = 0;
@@ -44,31 +45,118 @@ class OutputIO {
 
 class OutputIOLED : public OutputIO {
 
-  unsigned long startTimeMillis = 0;
-  unsigned long durationMillis = 1000;
+  int rPin = 9;
+  int gPin = 10;
+  int bPin = 11;
+
+  int inLength = 3;
+  uint32_t inColors[3] = {0x00FF00, 0x000000, 0x00FF00};
+  int inDurationsMillis[3] = {300, 300, 300};
+
+  int outLength = 3;
+  uint32_t outColors[3] = {0x0000FF, 0x000000, 0x0000FF};
+  int outDurationsMillis[3] = {300, 300, 300};
+
+  int nrLength = 5;
+  uint32_t nrColors[5] = {0xFF0000, 0x000000, 0xFF0000, 0x000000, 0xFF0000};
+  int nrDurationsMillis[5] = {500, 300, 500, 300, 500};
+
+  int length = 0;
+  uint32_t* colors = NULL;
+  int* durationsMillis = NULL;
+
+  unsigned long nextChangeTimeMillis = 0;
+
+  int index = 0;
 
   Command commandType = Command::NONE;
 
+  bool done = false;
+
+  void setColor(int r, int g, int b) {
+    analogWrite(rPin, r);
+    analogWrite(gPin, g);
+    analogWrite(bPin, b);
+  }
+
+  void setColor(uint32_t rgb) {
+    int r = (rgb >> 16) & 0xFF;   // Extract red (bits 16-23)
+    int g = (rgb >> 8) & 0xFF;  // Extract green (bits 8-15)
+    int b = rgb & 0xFF;          // Extract blue (bits 0-7)
+
+    Serial.print("R: ");
+    Serial.print(r);
+    Serial.print("  G: ");
+    Serial.print(g);
+    Serial.print("  B: ");
+    Serial.print(b);
+
+    analogWrite(rPin, r);
+    analogWrite(gPin, g);
+    analogWrite(bPin, b);
+
+  }
+
   public:
+    void createResources() {
+      pinMode(rPin, OUTPUT);
+      pinMode(gPin, OUTPUT);
+      pinMode(bPin, OUTPUT);
+    }
+
     void startCommand(Command command) override {
       commandType = command;
+      
+      index = 0;
+      done = false;
+      nextChangeTimeMillis = 0;
       Serial.print("Starting LED Command: ");
       Serial.println(commandType);
 
-      startTimeMillis = millis();
+      switch (commandType) {
+        case Command::SIGNED_IN:
+          colors = inColors;
+          durationsMillis = inDurationsMillis;
+          length = inLength;
+          break;
+        case Command::SIGNED_OUT:
+          colors = outColors;
+          durationsMillis = outDurationsMillis;
+          length = outLength;
+          break;
+        case Command::NOT_REGESTERED:
+          colors = nrColors;
+          durationsMillis = nrDurationsMillis;
+          length = nrLength;
+          break;
+      }
       return true;
     }
 
     void runCommand() override {
       Serial.println("Running LED Command");
+
+      if (millis() > nextChangeTimeMillis) {
+
+        if (index >= length) {
+          done = true;
+        } else {
+          setColor(colors[index]);
+          nextChangeTimeMillis = millis() + durationsMillis[index];
+          index++;
+        }
+        
+      }
+
     }
 
     bool isCommandFinished() override {
-        return startTimeMillis + durationMillis < millis();
+        return done;
     }
 
     void endCommand() override {
       Serial.println("Ending LED Command");
+      setColor(0, 0, 0);
     }
 };
 
@@ -91,8 +179,9 @@ Command update(OutputIO& output, Command command) {
     String input = Serial.readStringUntil('\n');
     command = parseInput(input);
 
+    output.endCommand();
+
     if (command != Command::NONE) {
-      output.endCommand();
       output.startCommand(command);
     }
   }
@@ -101,6 +190,7 @@ Command update(OutputIO& output, Command command) {
     output.runCommand();
 
     if (output.isCommandFinished()) {
+
       output.endCommand();
       command = Command::NONE;
     }
